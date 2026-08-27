89fb45d04ed41559ce4946107718a9dc744b59ee
anchorsendilnadar-max <anchorsendilnadar@gmail.com>
2026-08-27 09:44:11 +0530

    Refactor MainActivity to use AppCompatActivity
    Refactored MainActivity to extend AppCompatActivity and improved WebView initialization.

--- MainActivity.java
+++ MainActivity.java
@@ -1,21 +1,41 @@
 package com.globalbankrecoveryagency.app;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class MainActivity extends Activity {
-    @Override protected void onCreate(Bundle savedInstanceState) {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView web = new WebView(this);
+
+        web = new WebView(this);
+
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+
         web.setWebViewClient(new WebViewClient());
-        WebSettings s = web.getSettings();
-        s.setJavaScriptEnabled(true);
-        s.setDomStorageEnabled(true);
-        s.setAllowFileAccess(true);
+
         web.loadUrl("file:///android_asset/index.html");
+
         setContentView(web);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (web != null && web.canGoBack()) {
+            web.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
